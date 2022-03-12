5e3946e09261e9f479190991464f04ca7ccb8038
luc_char <luccharlopeau@gmail.com>
2022-03-12 15:31:32 +0100

    pop up filter, fixed typo

--- static/assets/js/tracker02.js
+++ static/assets/js/tracker02.js
@@ -1,7 +1,12 @@
-function openForm() {
-    document.getElementById("myForm").style.display = "block";
+openorclose = 0
+Form()
+function Form() {
+    if (openorclose == 1) {
+        document.getElementById("myForm").style.display = "block";
+        openorclose = 0
+    } else {
+        document.getElementById("myForm").style.display = "none";
+        openorclose = 1
+    }
 }
 
-function closeForm() {
-    document.getElementById("myForm").style.display = "none";
-}
