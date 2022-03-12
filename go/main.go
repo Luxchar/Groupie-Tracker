608708ddde119eccd878d86f42e8ae37fcbdfaf6
Luxchar <luccharlopeau@gmail.com>
2022-03-12 23:04:51 +0100

    map js lisibility improvement

--- go/main.go
+++ go/main.go
@@ -181,36 +181,11 @@ func JscriptStr() { //handles the map on the artist page
 		}
 		str = ""
 	}
+	s, _ := ioutil.ReadFile("../static/assets/js/mapjs.txt")
+	script := string(s)
 	k := `<script>
 	var LocationsForMap = [
-		` + strings.Join(slice, "\n") + `
-		];
-
-	// Initialize and add the map
-	function initMap() {
-	  // The map, centered 
-	  const map = new google.maps.Map(document.getElementById("map"), {
-		zoom: 1,
-		center: new google.maps.LatLng(36.91, 1.64),
-	  });
-
-	  var infowindow = new google.maps.InfoWindow();
-
-	  for (i = 0; i < LocationsForMap.length; i++) {
-		const marker = new google.maps.Marker({
-		  position: new google.maps.LatLng(LocationsForMap[i][1], LocationsForMap[i][2]),
-		  map: map,
-		});
-  
-		google.maps.event.addListener(marker, 'click', (function(marker, i) {
-		  return function() {
-			infowindow.setContent(LocationsForMap[i][0]+" "+LocationsForMap[i][3]);
-			infowindow.open(map, marker);
-		  }
-		})(marker, i));
-	  }
-	}
-  </script>`
+		` + strings.Join(slice, "\n") + script
 	artist.JsString = template.HTML(k)
 }
 
